d0164f2a0aa1754cf8cb32cc0684c3a11ca8b30b
rebeca-miguel <rebecagaspar19@gmail.com>
2022-09-11 02:36:31 -0300

    Atualizando o projeto final

--- index.js
+++ index.js
@@ -1,73 +1,74 @@
 // config inicial
 const express = require ('express')
 const app = express ()
-const mongoose = require('mongoose')
-const  cardapio = require('../models/cardapioModels')
+const mongoose = require('mongoose');
+const Cardapio = require('./models/Cardapio')
 
 
-// forma de ler JSON
+//forma de ler um json
+
 app.use(
     express.urlencoded({
-        extended: true,
+        extended:true
     })
 )
 
 app.use(express.json())
 
-//rotas da api
+//rotas da API
 
-app.post('/cardapioModels',async (req, res) => {
+app.post('./cardapio', async(req, res) => {
     //req.body
-    const {categoria, descricao,nome, preco,} = req.body
+    const {categoria, nome, descicao, preco, require} = req.body
+    //(categoria:alimento, nome:Pizza, descrição: calabresa, preço: 36,require:true)
+    
+    
+    if(!cardapio) {
+        res.status(422).json({error: 'O cardapio foi inserido com suceso'})
+    }
 
+    //criar atributo
     const cardapio = {
         categoria,
-        descricao,
         nome,
-        preco  
+        descricao,
+        preco,
+        require
     }
-
     try{
-        
-        cardapioModels.create(cardapioModels)
+        //criando dados
+        await Cardapio.create(cardapio)
 
-        res.status(201).json({message: 'produto criado com sucesso'}) 
-        
-        
+        res.status(201).json({message: 'Cardapio inserido com sucesso no sistema!'})
 
-    } catch (error) {
-        res.status(500).json({ error: error }) 
+    }   catch (error) {
+        res.status(500).json({error:error})
     }
-
 })
 
-//rota inicial / endepoint
-    app.get('/',(req, res)=>{
-    //mostrar requisição
-    res.json({message: 'oi Express'})
-})
+//rota inicial
+// app.get('/',(req, res) => {
+//     //mostrat rota
+//     res.json({message: 'olá express!'})
+// })
 
-const DB_USER = 'miguel'
-const DB_PASSWORD = encodeURIComponent('dOadyVlT7MjBqm19')
-//dOadyVlT7MjBqm19
+// //qD7WgblQMMmZy3F1
 
-//mongodb+srv://miguel:dOadyVlT7MjBqm19@apicluster2.eovpxpb.mongodb.net/?retryWrites=true&w=majority
+// //mongodb+srv://Rebeca:qD7WgblQMMmZy3F1@apicluster3.qvdwwhg.mongodb.net/bancodaapi?retryWrites=true&w=majority
 
-mongoose 
-    .connect(
-        'mongodb+srv://miguel:dOadyVlT7MjBqm19@apicluster2.eovpxpb.mongodb.net/?retryWrites=true&w=majority'
-    )
-    .then(() => {
+// //entregar a porta
+// const DB_USER = 'Rebeca'
+// const DB_PASSWORD = encodeURIComponent('qD7WgblQMMmZy3F1')
 
-        console.log("conectamos com o MongoDB!")
-        app.listen(3000)
+// mongoose
+// .connect(
+// `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster3.qvdwwhg.mongodb.net/bancodaapi?retryWrites=true&w=majority`,
+// )
+// .then(() => {
+//     console.log('conectamos com o Mongo!')
+//     app.listen(3000)
+// })
+// .catch((err) => console.log(err))
 
-    })
-    .catch((err) => console.log(err))
-
-// entregar uma porta
-
-app.listen(3000)
-console.log("conectamos com o MongoDB!")
 
 
